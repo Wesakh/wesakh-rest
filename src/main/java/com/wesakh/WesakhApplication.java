ba87729e073d90741a158205b333d224480f83f9
thaider <thaider@stellatechnology.com>
2018-12-14 15:45:24 +0500

    Blank wesakh rest

---
+++ src/main/java/com/wesakh/WesakhApplication.java
@@ -0,0 +1,14 @@
+package com.wesakh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WesakhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WesakhApplication.class, args);
+	}
+
+}
+
